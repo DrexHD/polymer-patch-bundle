dd0a48b2a1a50016498f1e778fcd7078f09db2c8
Drex <nicknamedrex@gmail.com>
2025-11-05 20:36:25 +0100

    Improve post visuals

--- src/main/java/me/drex/ppb/block/type/PostPolymerBlock.java
+++ src/main/java/me/drex/ppb/block/type/PostPolymerBlock.java
@@ -1,9 +1,10 @@
 package me.drex.ppb.block.type;
 
-import com.brand.blockus.blocks.base.PostBlock;
 import eu.pb4.factorytools.api.block.FactoryBlock;
 import eu.pb4.factorytools.api.block.model.generic.BSMMParticleBlock;
-import eu.pb4.factorytools.api.block.model.generic.ShiftyBlockStateModel;
+import eu.pb4.factorytools.api.block.model.generic.BlockStateModel;
+import eu.pb4.polymer.blocks.api.BlockModelType;
+import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils;
 import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
 import eu.pb4.polymer.virtualentity.api.ElementHolder;
 import net.minecraft.core.BlockPos;
@@ -12,32 +13,42 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.LightningRodBlock;
-import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import xyz.nucleoid.packettweaker.PacketContext;
 
 public record PostPolymerBlock() implements FactoryBlock, PolymerTexturedBlock, BSMMParticleBlock {
+    private static final BlockState[] EMPTY_STATES = new BlockState[6];
+
     public static final PostPolymerBlock INSTANCE = new PostPolymerBlock();
 
+    static {
+        int index = 0;
+        for (Direction.Axis axis : Direction.Axis.values()) {
+            for (boolean waterlogged : new boolean[]{false, true}) {
+                EMPTY_STATES[index] = PolymerBlockResourceUtils.requestEmpty(BlockModelType.getLightningRod(axis, waterlogged));
+                index++;
+            }
+        }
+    }
+
+
     @Override
-    public BlockState getPolymerBlockState(BlockState state, PacketContext context) {
-        BlockState blockState = Blocks.LIGHTNING_ROD.defaultBlockState();
-        Direction.Axis axis = state.getValue(PostBlock.AXIS);
-        var direction = switch (axis) {
-            case X -> Direction.EAST;
-            case Y -> Direction.UP;
-            default -> Direction.NORTH;
-        };
-        return blockState.setValue(LightningRodBlock.FACING, direction).setValue(LightningRodBlock.WATERLOGGED, state.getValue(SlabBlock.WATERLOGGED));
+    public BlockState getPolymerBlockState(BlockState blockState, PacketContext context) {
+        Direction.Axis axis = blockState.getValueOrElse(BlockStateProperties.AXIS, Direction.Axis.X);
+        Boolean waterlogged = blockState.getValueOrElse(BlockStateProperties.WATERLOGGED, false);
+
+        int i = axis.ordinal() * 2;
+        i += waterlogged ? 1 : 0;
+
+        return EMPTY_STATES[i];
     }
 
     @Override
     public @NotNull ElementHolder createElementHolder(ServerLevel world, BlockPos pos, BlockState initialBlockState) {
-        return ShiftyBlockStateModel.shortRange(initialBlockState, pos);
+        return BlockStateModel.shortRange(initialBlockState, pos);
     }
 
     @Override
